f84fe978d11502c70ac72fa240303a054d657ce4
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-07-20 17:17:29 -0300

    feat: Implementing person_finder_controller with tests

---
+++ src/controllers/person_creator_controller_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from .person_creator_controller import PersonCreatorController
+
+
+class MockPeopleRepository:
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int):
+        pass
+
+
+def test_create():
+    person_info = {"first_name": "John", "last_name": "Doe", "age": 30, "pet_id": 123}
+
+    controller = PersonCreatorController(MockPeopleRepository())
+    response = controller.create(person_info)
+
+    assert response["data"]["type"] == "Person"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == person_info
+
+
+def test_create_error():
+    person_info = {
+        "first_name": "John123",
+        "last_name": "Doe",
+        "age": 30,
+        "pet_id": 123,
+    }
+
+    controller = PersonCreatorController(MockPeopleRepository())
+
+    with pytest.raises(Exception):
+        controller.create(person_info)
---
+++ src/controllers/person_finder_controller.py
@@ -0,0 +1,36 @@
+from typing import Dict
+
+from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+
+
+class PersonFinderController:
+    def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
+        self.__people_repository = people_repository
+
+    def find(self, person_id: int) -> Dict:
+        person = self.__find_person_in_db(person_id)
+        response = self.__format_response(person)
+
+        return response
+
+    def __find_person_in_db(self, person_id: int) -> PeopleTable:
+        person = self.__people_repository.get_person(person_id)
+        if not person:
+            raise Exception("Pessoa não encontrada!")
+
+        return person
+
+    def __format_response(self, person: PeopleTable) -> Dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": {
+                    "first_name": person.first_name,
+                    "last_name": person.last_name,
+                    "pet_name": person.pet_name,
+                    "pet_type": person.pet_type,
+                },
+            }
+        }
