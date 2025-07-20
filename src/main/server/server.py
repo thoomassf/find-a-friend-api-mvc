edbd552ba69b42a8c2d943f74ed321926ad6cf10
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-07-20 18:16:27 -0300

    feat: Implementing first route (need fix)

---
+++ src/main/server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+from src.main.routes.pets_routes import pet_route_bp
+from src.models.sqlite.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(pet_route_bp)
